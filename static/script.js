617e0951ac02dac4d9b6c3c6a02fb4dc4f17e274
AndersonNavarro77 <165965300+AndersonNavarro77@users.noreply.github.com>
2024-05-26 17:58:22 -0300

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var windowHeight = window.innerHeight;
-    var headerHeight = document.querySelector("h1").offsetHeight;
-    var marginTop = (windowHeight / 2) - (headerHeight + 50); // 50px extra padding
-
-    document.querySelector("h1").style.marginTop = marginTop + "px";
-});
